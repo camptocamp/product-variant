e34475d9ce92db646f29bafcfd7338053675b694
Nils Coenen <nils.coenen@nico-solutions.de>
2025-05-14 09:42:07 +0200

    [FIX] product_variant_sale_price: Remove deprecated `pricelist` kwarg from _get_combination_info
    In Odoo 18, the keyword argument pricelist was removed from the _get_combination_info method. This PR removes the parameter from the calls to avoid a TypeError

--- product_variant_sale_price/models/product_product.py
+++ product_variant_sale_price/models/product_product.py
@@ -33,7 +33,6 @@ class ProductTemplate(models.Model):
         combination=False,
         product_id=False,
         add_qty=1,
-        pricelist=False,
         parent_combination=False,
         only_template=False,
     ):
@@ -41,7 +40,6 @@ class ProductTemplate(models.Model):
             combination,
             product_id,
             add_qty,
-            pricelist,
             parent_combination,
             only_template,
         )
